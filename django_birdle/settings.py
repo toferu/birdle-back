1442b9c66636bce7ad618c9d8ada5f176ae3d916
Christopher Anderson <tophermichaelson@gmail.com>
2023-01-23 05:01:09 -0500

    tweaking settings. mildly sweating

--- django_birdle/settings.py
+++ django_birdle/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['localhost', '.herokuapp.com']
+ALLOWED_HOSTS = ['localhost', 'birdlegotback.herokuapp.com']
 
 
 # Application definition
